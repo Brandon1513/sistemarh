5adff905fa3ea99822fa4c4773244707eedeced3
Brandon Devora <33169724+Brandon1513@users.noreply.github.com>
2024-11-20 16:26:46 -0600

    cambios en las vistas de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,47 +8,43 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Correo')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Contraseña')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block w-full mt-1"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
-
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
+                <input id="remember_me" type="checkbox" class="text-indigo-600 border-gray-300 rounded shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="text-sm text-gray-600 ms-2">{{ __('Recordarme') }}</span>
             </label>
         </div>
-        <br>
+        
+        <!-- Forgot Password Link -->
         <div class="flex justify-between my-5">
-            <x-link
-                :href="route('register')"
-            >
-                Crear Cuenta
-            </x-link>
-            <x-link
-            :href="route('password.request')"
-            >
-                Olvidaste tu contraseña
-            </x-link>
-
-            
+            <a href="{{ route('password.request') }}" 
+               class="flex items-center text-sm text-blue-600 hover:text-blue-800 hover:underline">
+                <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6 6m6 0l-6-6m-4 6h12" />
+                </svg>
+                {{ __('Olvidaste tu contraseña?') }}
+            </a>
         </div>
-        <br>
-        <x-primary-button class="w-full     justify-center">
-                {{ __('Iniciar Sesión') }}
+        
+
+        <!-- Login Button -->
+        <x-primary-button class="justify-center w-full">
+            {{ __('Iniciar Sesión') }}
         </x-primary-button>
     </form>
 </x-guest-layout>
