0b428dc75455510f156d374cb03a4a2b71352a2f
Brandon1513 <brandon.devora.lucio@gmail.com>
2025-02-25 15:23:02 -0600

    Crear periodos, para los empleados que cumplen aniversario de manera actumatica

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\PeriodoVacacionController;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('periodos:activar', function () {
+    $controller = new PeriodoVacacionController();
+    $controller->activarPeriodosAutomáticamente();
+    $this->info('Periodos de vacaciones revisados y activados correctamente.');
+})->describe('Activa automáticamente los periodos de vacaciones cuando un empleado cumple su aniversario');
